01e03c9d36ac6e61c4dbc08a270cde40aad0286e
shaanchanchani <shaan.chan3@gmail.com>
2023-05-16 09:34:05 -0500

    Setup

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st 
+import pandas as pd
+
+@st.cache_data
+def loadData():
+    df = pd.read_csv('FantasyPros_2022_Overall_ADP_Rankings.csv')
+    df['POS'] = df['POS'].str.replace('\d+', '', regex=True)
+    df = df[df['POS'].isin(['QB', 'RB', 'WR', 'TE'])]
+    df = df[['Player','Team','Bye','POS','AVG']]
+    df = df.dropna(how='all')
+    return df
+
+def initialize_teams(num_teams):
+    for i in range(num_teams):
+        positions = ['QB', 'RB1', 'RB2', 'WR1', 'WR2', 'TE', 'FLEX', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
+        if f'{i}' not in st.session_state: st.session_state[f'{i}'] = {pos: None for pos in positions}
+
+def handle_num_teams():
+    if st.session_state.num_teams_key:
+        st.session_state['num_teams'] = st.session_state.num_teams_key
+        st.session_state['user_first_pick'] = 0 
+
+def handle_user_first_pick():
+    if st.session_state.ufp_key:
+        st.session_state['user_first_pick'] = st.session_state.ufp_key
+
+def main():
+    APP_TITLE = 'Fantasy Football Snake Draft Optimizer'
+    st.set_page_config(APP_TITLE, layout = 'wide')
+    df = loadData()
+
+    if 'num_teams' not in st.session_state: st.session_state['num_teams'] = 0
+    if 'user_first_pick' not in st.session_state: st.session_state['user_first_pick'] = -1
+
+    if st.session_state['num_teams'] == 0:
+        st.number_input("How many teams are in your draft?", on_change = handle_num_teams, key = 'num_teams_key', step = 1, value = 0)
+
+    if st.session_state['user_first_pick'] == 0:
+        st.number_input("What slot are you drafting from?", on_change = handle_user_first_pick, key = 'ufp_key', step = 1, value = 0)
+
+    if st.session_state['num_teams'] != 0 and st.session_state['user_first_pick'] != 0:
+        st.write("Starting Draft")
+
+        initialize_teams(st.session_state['num_teams'])
+
+        draft_board_column, team_info_column = st.columns([3, 1])  # adjust the numbers to adjust column width
+
+        draft_board_column.header("Draft Board")
+        draft_board_column.dataframe(df, use_container_width = True)
+
+        # Use a selectbox to choose which team to display
+        team_to_display = team_info_column.selectbox('Select a team to display:', [i for i in range(1, st.session_state['num_teams'] + 1) if i != st.session_state['user_first_pick']])
+
+        for key, value in st.session_state[f'{team_to_display - 1}'].items():
+            if value is None:
+                team_info_column.write(key)
+            else:
+                team_info_column.write(value)
+
+if __name__ == "__main__":
+    main()
